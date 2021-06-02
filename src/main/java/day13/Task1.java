1a79f1e700ef13f7456ff1676d83f707c45e296e
Accbuyer <ilvevs@mail.ru>
2021-06-02 15:08:07 +0300

    complete day 13

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,36 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User user1 = new User("Max");
+        User user2 = new User("Alex");
+        User user3 = new User("Oleg");
+
+        user1.sendMessage(user2, "Привет!");
+        user1.sendMessage(user2, "Как дела?");
+
+        user2.sendMessage(user1, "Привет!");
+        user2.sendMessage(user1, "Хорошо");
+        user2.sendMessage(user1, "А твои как?");
+
+        user3.sendMessage(user1, "Добрый вечер!");
+        user3.sendMessage(user1, "Как на счёт прогуляться?");
+        user3.sendMessage(user1, "Например в 7 вечера");
+
+        user1.sendMessage(user3, "Добрый");
+        user1.sendMessage(user3, "Да, можно");
+        user1.sendMessage(user3, "Только давай попозже");
+
+        user3.sendMessage(user1, "Договорились");
+
+        MessageDatabase.showDialog(user1, user3);
+
+        user1.subscribe(user3);
+        user3.subscribe(user1);
+        System.out.println(user1.getSubscriptions());
+        System.out.println(user1.isFriend(user3));
+
+        System.out.println(MessageDatabase.getMessages());
+
 
     }
 }
---
+++ src/main/java/day13/User.java
@@ -0,0 +1,46 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String name;
+    private List <User> subscriptions;
+
+
+    public User(String name) {
+        this.name = name;
+        subscriptions  = new ArrayList<>();
+
+    }
+
+    public void subscribe(User user){
+        subscriptions.add(user);
+    }
+
+    public boolean isSubscribed(User user){
+        return subscriptions.contains(user);
+    }
+
+    public boolean isFriend(User user){
+        return this.getSubscriptions().contains(user) && user.getSubscriptions().contains(this);
+    }
+
+    public void sendMessage(User user, String text){
+        MessageDatabase.addNewMessage(this, user, text);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"  + name + '\'' +
+                '}';
+    }
+}
